4bd27fe69d78c8e72e14b477de81bff4eff98ad7
Anurag Sinha <anuraagrk4@gmail.com>
2024-07-20 17:25:56 +0530

    Fix: Fixed the mobile view for thank you page

--- src/Components/ThankYou.jsx
+++ src/Components/ThankYou.jsx
@@ -9,17 +9,16 @@ function ThankYou() {
 
 	return (
 		<section className='flex flex-col justify-center items-center gap-[50px] h-screen bg-[#002244] text-[#fff]'>
-			<div className='flex flex-col justify-center items-center'>
+			<div className='flex flex-col justify-center items-center '>
 				<div className='text-[24px] md:text-[40px]'>THANK YOU!</div>
 				<p className='border-b-[2px] pb-2'>
-					We have received your message and will get back to you
-					shortly.
+					We have received your message
 				</p>
 			</div>
 			<button
-				className='bg-[#eda22a] hover:bg-[#ff9b30] text-[#000] p-[10px] rounded-full text-[20px]'
+				className='bg-[#eda22a] w-[10rem] hover:bg-[#ff9b30] text-[#000] p-[10px] text-[20px]'
 				onClick={handleHomeClick}>
-				Home
+				HOME
 			</button>
 		</section>
 	);
